3543f753121868714c59bff649c4441f2b9b6cb3
Dustin Patterson <dustinrpatterson@gmail.com>
2016-10-26 11:23:14 -0600

    added socket

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ let express = require('express'),
   server = express(),
   port = process.env.PORT || 1582;
 
+//socket stuff
+let  http = require('http').Server(server),
+  io = require('socket.io')(http)
+
 //Registers Middleware for server
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
@@ -13,6 +17,16 @@ server.use('/', express.static(`${__dirname}/public/planets/`));
 server.use('/api', cors(handlers.corsOptions), routes.router)
 server.use('/', handlers.defaultErrorHandler)
 
-server.listen(port, function () {
+//socket connection
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  setInterval(() => {
+    io.emit('update','the time is: ' + new Date().getTime());
+  }, 1000);
+
+});
+
+http.listen(port, function () {
   console.log(`Creating worlds on port: ${port}`);
 })
