4a58b638d3fd9081f7ef2b6361a53f9b6dd1838e
Dustin Patterson <dustinrpatterson@gmail.com>
2016-10-27 10:09:23 -0600

    updated URL for heroku get

--- public/planets/service.js
+++ public/planets/service.js
@@ -2,7 +2,7 @@ function UniverseService(){
 
 this.getApiGalaxyData = function loadGalaxyData(cb){
 
-var apiUrl = "http://localhost:1582/api/"; 
+var apiUrl = "http://dpgalaxy.herokuapp.com/api/"; 
 var galaxyApi = "galaxies";
 var starApi = "stars";
 var planetApi = "planets";
@@ -10,7 +10,7 @@ var moonApi = "moons";
 var creatureApi = "creatures";
 var testApi = 'http://localhost:1582/api/galaxies/?include=stars,planets,moons'
 
-$.getJSON(testApi, function(data){
+$.getJSON(apiUrl+galaxyApi, function(data){
     galaxyData = data.body;
     console.log('galaxy data is here')
     cb(data)
